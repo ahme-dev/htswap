e557cec3159d9da4473999df85ece35487f18a70
Ahmed <mail@ahme.dev>
2025-08-15 16:16:58 +0300

    chore: rename dist dirs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,18 +1,30 @@
 import { defineConfig } from "tsdown";
 
-// "build": "tsdown ./src/index.ts -d dist-es6 --platform browser --target es6 --format cjs,esm --dts;",
 export default defineConfig([
 	{
+		name: "Compatiblity",
 		entry: "./src/index.ts",
-		outDir: "dist-es2015",
+		outDir: "dist-compat",
 		platform: "browser",
 		target: "es2015",
 		format: ["commonjs", "esm"],
 		dts: true,
 	},
 	{
+		name: "Modern",
 		entry: "./src/index.ts",
-		outDir: "dist-es2020",
+		outDir: "dist",
+		target: "es2020",
+		minify: true,
+		platform: "browser",
+		format: ["commonjs", "esm", "es"],
+		dts: true,
+	},
+	{
+		name: "Modern Minified",
+		entry: "./src/index.ts",
+		outDir: "dist-min",
+		minify: true,
 		target: "es2020",
 		platform: "browser",
 		format: ["commonjs", "esm", "es"],
